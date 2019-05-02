976b8483ea6f70e77a3869b74cb2b6ebf3e8a10d
Michael Henderson <mhenderson557@yahoo.com>
2019-05-01 23:36:05 -0500

    Updates

---
+++ precache-manifest.a814cbe27df06040810cca7405cfd11a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30b4b70a956b99beefd332104df446f",
+    "url": "/googlesarchbooks/index.html"
+  },
+  {
+    "revision": "bb53dca6367d48cea6bd",
+    "url": "/googlesarchbooks/static/css/main.84576519.chunk.css"
+  },
+  {
+    "revision": "259cea7aa902c7ce0167",
+    "url": "/googlesarchbooks/static/js/2.9153a2ba.chunk.js"
+  },
+  {
+    "revision": "bb53dca6367d48cea6bd",
+    "url": "/googlesarchbooks/static/js/main.5fee4e95.chunk.js"
+  },
+  {
+    "revision": "32c635bef2eef6d7c65f",
+    "url": "/googlesarchbooks/static/js/runtime~main.7943ef24.js"
+  }
+]);
